36fae9cd66bb81cd0fdf216dee652f585fea2e96
girum54 <girumtilahun54@gmail.com>
2024-10-16 12:33:39 +0300

    main App component using Auth context for proper user token handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,49 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
+import UserAuth from "./userAuth/userAuth";
+import AddEvent from "./addEvent/addEvent";
+import UpcomingEvents from "./upcomingEventsList/upcomingEvents";
+import { AuthProvider, AuthContext } from "./userAuth/AuthContext";
+
+const AuthenticatedApp = () => (
+  <div>
+    <AuthContext.Consumer>
+      {({ handleLogout }) => (
+        <button className="btn-primary" onClick={handleLogout}>
+          Logout
+        </button>
+      )}
+    </AuthContext.Consumer>
+    <Routes>
+      <Route path="/addevent" element={<AddEvent />} />
+      <Route path="/upcomingevents" element={<UpcomingEvents />} />
+      <Route path="*" element={<Navigate to="/upcomingevents" />} />
+    </Routes>
+  </div>
+);
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AuthProvider>
+      <Router>
+        <AuthContext.Consumer>
+          {({ isLoggedIn, handleLogin }) =>
+            isLoggedIn ? (
+              <AuthenticatedApp />
+            ) : (
+              <UserAuth setIsLoggedIn={handleLogin} />
+            )
+          }
+        </AuthContext.Consumer>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;

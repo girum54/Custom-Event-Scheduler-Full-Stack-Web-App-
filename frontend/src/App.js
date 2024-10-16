fc40b175e52bbc80ac3a9d5d279865558f434182
girum54 <girumtilahun54@gmail.com>
2024-10-16 20:05:25 +0300

    main App routes updated to include edit event, edit event is only accesible from upcoming events list for now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
+  Link,
 } from "react-router-dom";
 import "./App.css";
 import UserAuth from "./userAuth/userAuth";
 import AddEvent from "./addEvent/addEvent";
 import UpcomingEvents from "./upcomingEventsList/upcomingEvents";
+import ViewAllEvents from "./viewAllEventsCalendar/ViewAllEvents";
+import EditEvent from "./editEvent/editEvent"; // Import EditEvent component
 import { AuthProvider, AuthContext } from "./userAuth/AuthContext";
 
-const AuthenticatedApp = () => (
-  <div>
-    <AuthContext.Consumer>
-      {({ handleLogout }) => (
-        <button className="btn-primary" onClick={handleLogout}>
-          Logout
-        </button>
-      )}
-    </AuthContext.Consumer>
-    <Routes>
-      <Route path="/addevent" element={<AddEvent />} />
-      <Route path="/upcomingevents" element={<UpcomingEvents />} />
-      <Route path="*" element={<Navigate to="/upcomingevents" />} />
-    </Routes>
-  </div>
-);
+const AuthenticatedApp = () => {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  return (
+    <div>
+      <AuthContext.Consumer>
+        {({ handleLogout }) => (
+          <div>
+            <nav className="navbar">
+              <button
+                className="btn-primary nav-toggle"
+                onClick={handleNavCollapse}
+              >
+                ☰
+              </button>
+              <div className={`${isNavCollapsed ? "collapse" : ""} nav-links`}>
+                <Link to="/addevent" className="nav-link btn-primary">
+                  Add Event
+                </Link>
+                <Link to="/upcomingevents" className="nav-link btn-primary">
+                  Upcoming Events
+                </Link>
+                <Link to="/viewallevents" className="nav-link btn-primary">
+                  View All Events
+                </Link>
+                <button className="nav-link btn-primary" onClick={handleLogout}>
+                  Logout
+                </button>
+              </div>
+            </nav>
+            <Routes>
+              <Route path="/addevent" element={<AddEvent />} />
+              <Route path="/upcomingevents" element={<UpcomingEvents />} />
+              <Route path="/viewallevents" element={<ViewAllEvents />} />
+              <Route path="/editevent/:eventId" element={<EditEvent />} />
+              <Route path="*" element={<Navigate to="/upcomingevents" />} />
+            </Routes>
+          </div>
+        )}
+      </AuthContext.Consumer>
+    </div>
+  );
+};
 
 function App() {
   return (
